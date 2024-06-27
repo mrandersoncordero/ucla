006b97f8b224927c837f89b1caa095adb7899a94
mrandersoncordero <acorderofigueroa7@gmail.com>
2024-06-27 19:08:08 -0400

    ejercicio 7

---
+++ IntroComputacion/Corte1/7/models/CuentaBancaria.js
@@ -0,0 +1,36 @@
+export default class CuentaBancaria {
+  constructor(saldoInicial, montoTotalDeposito, montoTotalRetiro) {
+    this._saldoInicial = parseInt(saldoInicial);
+    this._montoTotalDeposito = parseInt(montoTotalDeposito);
+    this._montoTotalRetiro = parseInt(montoTotalRetiro);
+  }
+
+  get saldoInicial() {
+    return this._saldoInicial;
+  }
+
+  get montoTotalDeposito() {
+    return this._montoTotalDeposito;
+  }
+
+  get montoTotalRetiro() {
+    return this._montoTotalRetiro;
+  }
+
+  set saldoInicial(dato) {
+    this._saldoInicial = dato;
+  }
+
+  set montoTotalDeposito(dato) {
+    this._montoTotalDeposito = dato;
+  }
+
+  set montoTotalRetiro(dato) {
+    this._montoTotalRetiro = dato;
+  }
+
+  comision() {
+    let comision = this.montoTotalRetiro * .05;
+    return comision;
+  }
+}
